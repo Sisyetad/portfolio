0c7566d4cc126e943a3322f8e40a2d96080c99f8
Sisyetad <sisaytadewos@gmail.com>
2026-09-14 08:49:01 +0300

    visitors

---
+++ portfolio/.next/server/app/api/visitor-count/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/visitor-count/route.js")
+R.c("server/chunks/[externals]__1udvxjy._.js")
+R.c("server/chunks/[root-of-the-server]__0db8dwe._.js")
+R.c("server/chunks/_next-internal_server_app_api_visitor-count_route_actions_19oatuz.js")
+R.m(12238)
+module.exports=R.m(12238).exports
